4d0123dbd3fc93b5131c7fcd97547a907930321b
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-25 11:36:15 +0530

    🔥 refactor(App.js): remove commented code and unused variable to improve code readability and maintainability 🚀 perf(App.js): use createBrowserRouter with an array of routes instead of createRoutesFromElements to improve performance and simplify code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+
+// const routerDefination = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/products", element: <Products /> },
 ]);
 
-function App() {  
+// const router = createBrowserRouter(routerDefination);
+
+function App() {
   return <RouterProvider router={router} />;
 }
 
